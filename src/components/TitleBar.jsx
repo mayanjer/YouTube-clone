082eb4aa05b23346cd3d3a66f1ce1e13dfec2309
mayanja <mayanjer3@gmail.com>
2026-01-22 08:59:38 +0300

    added lucid react

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,15 +1,19 @@
 
 import menuLogo from '../assets/menu-symbol.svg'
 import youtubeLogo from '../assets/youtube-logo.svg'
+import { Search } from 'lucide-react'
 
 
 export default function TitleBar() {
     return <span className="flex gap-2">
+        
         <img
             src={menuLogo}
             alt=" "
-            className="w-10 h-5" />
+            className="w-10 h-5 mt-2" />
         <img src={youtubeLogo} alt=""
-        className="w-25"/>
+            className="w-25" />
+        
+        <input type="text" className="border w-100 h-10 mx-auto rounded-2xl px-4"/>
     </span>
 }
