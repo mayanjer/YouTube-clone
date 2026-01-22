5cf4bfa3086b016aba9121cec553ba79232c5551
mayanja <mayanjer3@gmail.com>
2026-01-22 16:22:08 +0300

    added the 'you' section

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,14 @@
-import { House, Clapperboard, Youtube, UserPen} from "lucide-react";
+import {
+  House,
+  Clapperboard,
+  Youtube,
+  UserPen,
+  History,
+  ListMusic,
+  Clock,
+  ThumbsUp,
+} from "lucide-react";
 import { SUBSCRIPTIONS } from "../data";
-
 
 export default function SideBar() {
   // simple sidebar rendered initially
@@ -32,7 +40,7 @@ export default function SideBar() {
 
   // detailed sidebar rendered on click of the menu icon
   const detailedSideBar = (
-    <section className="ml-6 mt-4 flex flex-col w-55">
+    <section className="ml-6 mt-4 flex flex-col w-55 text-sm">
       <span className="flex gap-8 w-45 h-10  items-center hover:bg-gray-300 rounded-xl px-1">
         <House />
         Home
@@ -50,7 +58,7 @@ export default function SideBar() {
       </h5>
       <div>
         {SUBSCRIPTIONS.map(({ name, imageSource }) => (
-          <p className="flex gap-8 items-center  text-sm hover:bg-gray-300 h-10 rounded-xl">
+          <p className="flex gap-8 items-center hover:bg-gray-300 h-10 rounded-xl">
             <span className="rounded-full h-7 w-7 overflow-hidden">
               <img src={imageSource} className="h-full w-full" />
             </span>
@@ -59,10 +67,31 @@ export default function SideBar() {
         ))}
       </div>
       <hr className="w-55 border-gray-300 mt-5 -ml-6" />
+
       {/* You */}
-      <div>
-        <p>hello world!</p>
+      <div className="flex flex-col gap-3">
+        <h5 className="flex gap-4 hover:bg-gray-300 rounded-xl h-10 items-center px-1 mt-2">
+          <span className="font-bold">You</span>
+          {">"}
+        </h5>
+        <p className="flex gap-8">
+          <History /> History
+        </p>
+        <p className="flex gap-8">
+          <ListMusic /> Play Lists
+        </p>
+        <p className="flex gap-8">
+          <Clock /> Watch Later
+        </p>
+        <p className="flex gap-8">
+          <ThumbsUp /> Liked Videos
+        </p>
+        <p className="flex gap-8">
+          <Youtube /> Your Videos
+        </p>
       </div>
+
+      {/* explore */}
     </section>
   );
 
