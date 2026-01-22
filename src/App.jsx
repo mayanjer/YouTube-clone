33ecd22bf216435d77377c6072df11b70c2658cb
mayanja <mayanjer3@gmail.com>
2026-01-22 09:49:43 +0300

    enhanced the icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import TitleBar from "./components/TitleBar";
 
 function App() {
-  return <div className="mt-5 ml-5">
+  return <div className="mt-5 ml-5 mr-5">
   <TitleBar/>
   </div>;
 }
--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,11 +1,11 @@
 import menuLogo from "../assets/menu-symbol.svg";
 import youtubeLogo from "../assets/youtube-logo.svg";
-import { Mic, Bell, Menu, Circle } from "lucide-react";
+import { Mic, Bell, Menu, UserPen, Plus } from "lucide-react";
 
 export default function TitleBar() {
   return (
     <span className="flex gap-2">
-      <Menu className="mt-2"/>
+      <Menu className="mt-2" />
       <img src={youtubeLogo} alt="" className="w-25" />
       <span className="flex gap-3 mx-auto">
         <input
@@ -13,15 +13,22 @@ export default function TitleBar() {
           className="border w-100 h-10  rounded-2xl px-4"
           placeholder="Search"
         />
-
-        <Mic className="mt-2" />
-          </span>
-          <span className="flex gap-2">
-              <button className="bg-gray-100">Create</button>
-              <Bell className="mt-2" />
-              <Circle className="mt-2"/>
-
-          </span>
+        <span className="bg-gray-100 hover:bg-gray-200 rounded-full w-10 flex justify-center">
+          <Mic className="mt-2" />
+        </span>
+      </span>
+      <span className="flex gap-5">
+        <button className="bg-gray-100 font-bold p-2 flex gap-2 rounded-2xl hover:bg-gray-200">
+          <Plus />
+          Create
+        </button>
+        <Bell className="mt-2 rounded-full  hover:bg-gray-200" />
+        <div className="border rounded-full w-10 flex justify-center">
+          <div className="flex flex-col justify-center">
+            <UserPen />
+          </div>
+        </div>
+      </span>
     </span>
   );
 }
