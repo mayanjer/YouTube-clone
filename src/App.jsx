556d2b2fe8ba4ba01889a42a0a45ca00783f8ed5
mayanja <mayanjer3@gmail.com>
2026-01-29 10:23:35 +0300

    setting the main content on the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import MainContent from "./components/MainContent";
 
 function App() {
   return (
-    <div className="mt-2 mr-5 grid grid-cols-[200px_1fr_1fr]">
+    <div className="mt-2 mr-5 grid grid-cols-[200px_1fr_1fr] grid-rows-5">
       <SideBar />
       <div className="col-span-2">
         <TitleBar />
       </div>
-      <div>
+      <div className="col-start-2">
         <MainContent />
       </div>
     </div>
