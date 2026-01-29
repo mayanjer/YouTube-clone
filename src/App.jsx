deb6f01516eb16b5af1c2abcbd401f2a20f75050
mayanja <mayanjer3@gmail.com>
2026-01-29 10:38:54 +0300

    fighting with the display

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import MainContent from "./components/MainContent";
 
 function App() {
   return (
-    <div className="mt-2 mr-5 grid grid-cols-[200px_1fr_1fr] grid-rows-5">
-      <SideBar />
-      <div className="col-span-2">
-        <TitleBar />
+    <>
+      <TitleBar />
+
+      <div className="mt-2 mr-5 grid grid-cols-[200px_1fr_1fr] grid-rows-5">
+        <div className="row-start-1 col-start-1">
+
+        <SideBar />
+        </div>
+
+        <div className="col-start-2 space-y-2 row-start-1">
+          <MainContent />
+        </div>
       </div>
-      <div className="col-start-2">
-        <MainContent />
-      </div>
-    </div>
+    </>
   );
 }
 
