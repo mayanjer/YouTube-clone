77e456a1eb74a46bcbbdfdfb2f4b982f344346b0
mayanja <mayanjer3@gmail.com>
2026-02-02 12:16:14 +0300

    finalized adding dummy data

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,9 +7,9 @@ export default function Post({
   timePosted,
 }) {
   return (
-    <div className="border rounded-2xl min-w-60 min-h-90">
+    <div className="rounded-2xl min-w-60 min-h-90">
       <div
-        className="border rounded-2xl h-70 overflow-hidden hover:bg-gray-200 hover:min-w-62 flex justify-center
+        className="rounded-2xl h-70 overflow-hidden hover:bg-gray-200 hover:min-w-62 flex justify-center
       transform transition duration-250 ease-out hover:scale-102
       "
       >
@@ -17,7 +17,7 @@ export default function Post({
       </div>
 
       <p className="flex gap-2 mt-3 font-bold">
-        <div className="border h-10 w-10 rounded-full overflow-hidden ">
+        <div className="h-10 w-10 rounded-full overflow-hidden ">
           <img src={profilePicture} alt="" className="h-full w-full" />
         </div>
         {heading}
